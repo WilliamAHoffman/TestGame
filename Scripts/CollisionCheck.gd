7403f83f04ee0d28642eb58d2c1cb8bf25bd8ac5
William Hoffman <whoffman4230@gmail.com>
2024-05-22 22:41:52 -0500

    added clam rotation

---
+++ Scripts/CollisionCheck.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+#enemy(kills player)
+#player(dies to enemy)
+@export var entityType: String
+
+func _process(delta):
+	pass
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,24 +2,33 @@ extends CharacterBody2D
 
 @export var movement: Movement
 
-func _process(delta):
-	
+func move():
 	movement.direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("up") && Input.is_action_pressed("down"):
-		movement.direction = Vector2(movement.direction.x,0)
+		movement.direction.y = 0
 	elif Input.is_action_pressed("up"):
-		movement.direction = Vector2(movement.direction.x,-1);
+		movement.direction.y = -1
 	elif Input.is_action_pressed("down"):
-		movement.direction = Vector2(movement.direction.x,1);
+		movement.direction.y = 1
 
 	if Input.is_action_pressed("left") && Input.is_action_pressed("right"):
-		movement.direction = Vector2(0,movement.direction.y)
+		movement.direction.x = 0
 	elif Input.is_action_pressed("left"):
-		movement.direction = Vector2(-1,movement.direction.y);
+		movement.direction.x = -1
 	elif Input.is_action_pressed("right"):
-		movement.direction = Vector2(1,movement.direction.y);
+		movement.direction.x = 1
+
+func playerRotation():
+	var target_position = get_global_mouse_position()
+	var entity_position = global_position
+	var mouse_local = Vector2(target_position.x - entity_position.x, target_position.y - entity_position.y)
+	global_rotation = mouse_local.angle()
 	
+
+func _process(delta):
+	move()
+	playerRotation()
 
 func _physics_process(delta):
 	move_and_slide()
